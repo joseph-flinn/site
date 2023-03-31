03fed6b9b3403c6822e451eecacba70ce4a51c58
Joseph Flinn <joseph.s.flinn@gmail.com>
2023-03-30 18:26:22 -0700

    Adding a theme file for later

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+  "typography": {
+    "fontFamily": "Monoid",
+    "fontSize": 14,
+  },
+  "page": {
+    "backgroundColor": {
+      "primary": "#fffefb"
+    },
+  },
+  "palette": {
+    "primary": {
+      "main": "#5c5955"
+    }
+  }
+}
