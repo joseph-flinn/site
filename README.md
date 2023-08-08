c1e96f8caeeb3bc0c7e3acc23efa71463e6a15dc
Joseph Flinn <58369717+joseph-flinn@users.noreply.github.com>
2023-08-08 06:28:35 -0700

    Refactor to env files (#21)
    * Improve quality of life for checking post format
    
    * Fixing the .env files (no secrets in them)
    
    * Update docs

--- README.md
+++ README.md
@@ -1,18 +1,8 @@
 # site - Indie Web: Source of Truth
 
-## Development
+## Frontend
 
-```bash
-nix-shell
-vite dev  # Run a local development instance at http://localhost:5173
+## Backend
 
+## Data
 
-# Run a local development instance across all networking interfaces on port 5173. 
-# This is used to expose via Tailscale or the VLAN the computer is on. Mostly used
-# for confirming mobile UX
-vite dev --host 0.0.0.0  
-```
-
-
-## Deployment
-Every push to `main` will auto deploy to `joseph-flinn.github.io/site`
--- frontend/src/lib/helpers.js
+++ frontend/src/lib/helpers.js
@@ -1,14 +1,39 @@
-import { config } from '$lib/config.json';
+// import { config } from '$lib/config.json';
+import { 
+    PUBLIC_DATASOURCE_TYPE,  
+    PUBLIC_DATASOURCE,
+    PUBLIC_LOGGING_ENABLED
+} from '$env/static/public';
+
 
 let POSTS_CACHE = null;
 
-export const getPosts = (fetch) => {
-    if (POSTS_CACHE !== null) return POSTS_CACHE;
 
-    return fetch(`${config.dataUrl}/posts.json`)
+export const getPosts = (fetch) => {
+    log('$lib.helpers:getPosts()', `Datasource Type: ${PUBLIC_DATASOURCE_TYPE}`)
+
+    if (POSTS_CACHE !== null) {
+        log('$lib.helpers:getPosts()', "posts cache hit")
+        return POSTS_CACHE
+    };
+
+    if (PUBLIC_DATASOURCE_TYPE == 'local') {
+       return import('../posts.json').then(posts => {
+           POSTS_CACHE = posts.default
+       }).then(() => POSTS_CACHE);
+    }
+
+    return fetch(`${PUBLIC_DATASOURCE}/posts.json`)
         .then(response => response.json())
         .then(resp => {
             POSTS_CACHE = resp;
         })
         .then(() => POSTS_CACHE);
+};
+
+
+export const log = (obj, message) => {
+    if (PUBLIC_LOGGING_ENABLED == 'true') {
+        console.log(`${obj} => ${message}`)
+    }
 };
