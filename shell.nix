5120d6d7b9dccb42c473ca28e8fadeba8dba4324
Joseph Flinn <joseph.s.flinn@gmail.com>
2023-08-14 16:12:54 -0700

    Add conditional deploys. Update dev environment

--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@ stdenv.mkDerivation {
     name = "node";
     buildInputs = [
         nodejs
+        actionlint
     ];
     shellHook = ''
         export PATH="$PWD/frontend/node_modules/.bin/:$PATH"
