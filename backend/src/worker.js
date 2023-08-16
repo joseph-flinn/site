0959b17851838119e7e76a248ad492485eb1a176
Joseph Flinn <joseph.s.flinn@gmail.com>
2023-08-16 07:04:08 -0700

    Initial commit of updating worker to support /posts endpoint

--- backend/src/worker.js
+++ backend/src/worker.js
@@ -10,23 +10,62 @@
 
 export default {
   async fetch(request, env) {
-    // For example, the request URL my-worker.account.workers.dev/image.png
+		const headers = new Headers();
+		headers.set('Access-Control-Allow-Origin', '*');
+
     const url = new URL(request.url);
     const key = url.pathname.slice(1);
-    // Retrieve the key "image.png"
-    const object = await env.BLOG_BUCKET.get(key);
+		console.log(`key: ${key}`)
 
-    if (object === null) {
-      return new Response('Object Not Found', { status: 404 });
-    }
+		const routeRSS = /rss.xml/,
+			    routePostList = /posts$/,
+				  routePost = /posts\/*/;
 
-    const headers = new Headers();
-    object.writeHttpMetadata(headers);
-    headers.set('etag', object.httpEtag);
-    headers.set('Access-Control-Allow-Origin', '*');
+		switch (true) {
+			case routeRSS.test(key):
+				console.log(`RSS match found`)
+				const rssObject = await env.BLOG_BUCKET.get(key);
 
-    return new Response(object.body, {
-      headers,
-    });
+				if (rssObject === null) {
+					return new Response('Object Not Found', { status: 404 });
+				}
+
+				rssObject.writeHttpMetadata(headers);
+				headers.set('etag', object.httpEtag);
+
+				return new Response(rssObject.body, {
+					headers,
+				});
+				break;
+			case routePostList.test(key):
+				const postListObject = await env.BLOG_BUCKET.get('posts.json');
+
+				if (postListObject === null) {
+					return new Response('Object Not Found', { status: 404 });
+				}
+
+
+				headers.set('etag', postListObject.httpEtag);
+
+				return new Response(postListObject.body, {
+					headers,
+				});
+				break;
+			case routePost.test(key):
+				return new Response(JSON.stringify({ message: "Post match found" }, null, 2), {
+					headers,
+				});
+				break;
+			default:
+				headers.set('Accept', 'application/json')
+				const response = {
+					message: `${key} not found`
+				}
+				return new Response(JSON.stringify(response, null, 2), {
+					status: 404,
+					headers,
+				});
+				break;
+		}
   },
 };
