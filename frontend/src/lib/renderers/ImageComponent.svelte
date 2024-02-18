1f838f0e481517f966b80a0e9543eed9fa3f1799
Joseph Flinn <joseph.s.flinn@gmail.com>
2024-02-18 11:04:49 -0800

    Part one of solving the PR review timeline issues. Added support for images in markdown

---
+++ frontend/src/lib/renderers/ImageComponent.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let href = "";
+  export let title = undefined;
+  export let text = "";
+
+  const imgUrl = href.startsWith("http") ? href : `/src/lib${href}`
+</script>
+
+<div>
+  <img
+    src={imgUrl}
+    alt={text}
+    class="center"
+  />
+  <div class="imgTitle">{text}</div>
+</div>
+
+
+<style>
+  .center {
+    display: block;
+    margin-right: auto;
+    margin-left: auto;
+    margin-top: 2em;
+    margin-bottom: 0.5em;
+  }
+
+  .imgTitle {
+    font-size: 12px;
+    margin-bottom: 2em;
+    text-align: center;
+  }
+</style>
