fa19415d981c1b1a2e2bfe822b975939cc0c08d3
Joseph Flinn <joseph.s.flinn@gmail.com>
2024-10-24 09:55:44 -0700

    Update Socials for Desktop site

--- frontend/src/lib/components/Socials.svelte
+++ frontend/src/lib/components/Socials.svelte
@@ -10,11 +10,38 @@
 
 <div style="display: flex; padding: 2em; text-align: center;">
   {#each data as link}
-    <a style="flex: 1;" href={link.link}>
-      <i class="fa {link.icon}"/>
-      {#if link.extra != ""}
-       {link.extra}
-      {/if}
-    </a>
+    <div 
+      class="social-button"
+      on:click={() => {
+        goto(link.link).then(() => {})
+      }}
+    >
+      <a href={link.link}>
+        <i class="fa {link.icon}"/>
+        {#if link.extra != ""}
+         {link.extra}
+        {/if}
+      </a>
+    </div>
   {/each}
 </div>
+
+<style>
+  a:hover {
+    text-decoration: none; 
+  }
+
+  .social-button {
+    flex: 1;
+    margin-left: auto;
+    padding: 0.5em 1.5em 0.5em 1.5em;
+    text-align: center;
+    border-radius: 5px;
+    border: none;
+  }
+
+  .social-button:hover {
+    cursor: pointer;
+    background: #eee;
+  }
+</style>
