57eed18b054ceee8ba684493667a9844321cb52c
Joseph Flinn <joseph.s.flinn@gmail.com>
2024-10-24 09:35:48 -0700

    Add socials

---
+++ frontend/src/lib/components/Socials.svelte
@@ -0,0 +1,20 @@
+<script>
+    const data = [
+        {"link":"https://linkedin.com/in/joseph-flinn", "icon": "fa-linkedin-square", "extra": ""},
+        {"link":"https://github.com/joseph-flinn", "icon": "fa-github", "extra": ""},
+        {"link":"https://blog-images.flinnlab.com/resume-joseph-flinn.pdf", "icon": "fa-download", "extra": "CV"},
+    ];
+
+</script>
+
+
+<div style="display: flex; padding: 2em; text-align: center;">
+  {#each data as link}
+    <a style="flex: 1;" href={link.link}>
+      <i class="fa {link.icon}"/>
+      {#if link.extra != ""}
+       {link.extra}
+      {/if}
+    </a>
+  {/each}
+</div>
--- frontend/src/routes/(site)/+page.svelte
+++ frontend/src/routes/(site)/+page.svelte
@@ -4,21 +4,20 @@
   import Toggle from "$lib/components/Toggle.svelte";
 
   import TabbedCard from "$lib/components/TabbedCard.svelte";
+  import Socials from "$lib/components/Socials.svelte";
   import UnderConstruction from "$lib/components/UnderConstruction.svelte";
 
   import data from '$lib/assets/data.json';
 
 </script>
 
-
-
-<div class="titleWrapper">
-  <PageTitle name="skills"/>
-</div>
-<TabbedCard data={data.skillsData}>
-</TabbedCard>
-
-<div style="padding: 2em; text-align: right;">
-    <a href="https://blog-images.flinnlab.com/resume-joseph-flinn.pdf">Resume (PDF)</a>
+<div>
+  <div class="titleWrapper">
+    <PageTitle name="skills"/>
+  </div>
+  <TabbedCard data={data.skillsData}/>
 </div>
 
+<div style="flex-grow: 1;"/>
+
+<Socials/>
