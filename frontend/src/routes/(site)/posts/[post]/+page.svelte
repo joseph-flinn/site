12d09b172d0e2e2b668b150eb41ed03fa144663a
Joseph Flinn <joseph.s.flinn@gmail.com>
2025-01-20 12:25:27 -0800

    [feat] Add OGP to blog posts

--- frontend/src/routes/(site)/posts/[post]/+page.svelte
+++ frontend/src/routes/(site)/posts/[post]/+page.svelte
@@ -1,16 +1,19 @@
 <script>
   export let data;
 
-  import SvelteMarkdown from 'svelte-markdown';
-
   import { log } from '$lib/utils/logger.js';
   import TailwindSvelteMarkdown from '$lib/components/TailwindSvelteMarkdown.svelte';
 
-  const { title, published, body, ...rest } = data.data;
+  const { title, published, body, description, ...rest } = data.data;
   const wordCount = body.split(" ").reduce((sum, word) => sum += (word != "") ? 1 : 0, 0);
   const readEstimate = Math.round( wordCount / 200)
 </script>
 
+<svelte:head>
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta property="og:url" content={window.location.href} />
+</svelte:head>
 
 <div class="w-full text-center text-xl">
   <div class="p-4">
