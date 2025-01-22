bf0644f1ce50fee74788e608cb618a0f8c868225
Joseph Flinn <joseph.s.flinn@gmail.com>
2025-01-21 17:35:08 -0800

    Removing CMS and CMS components from the blog

--- frontend/src/routes/(cms)/cms/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  import { goto } from '$app/navigation'
-  import { base } from '$app/paths'
-
-  goto(`${base}/cms/drip`)
-</script>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script>
+  import "../app.css";
+
+  import { base } from '$app/paths'
   import { page } from '$app/stores';
 
-  import "../app.css";
+  import CentralColumn from '$lib/layouts/CentralColumn.svelte';
+  import HorizontalNav from '$lib/components/HorizontalNav.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+
   import { getImageUrl } from '$lib/utils/loader.js'
+  import data from '$lib/assets/data.json';
 
   $: pageData = $page.data.data ?? {}
 </script>
@@ -14,7 +21,13 @@
   <meta property="og:title" content={ pageData.title ?? "joseph.flinnlab.com"} />
   <meta property="og:image" content={getImageUrl('/jf-icon.png')} />
   <meta property="og:description" content={ pageData.description ?? "Joseph Flinn's personal website that includes a blog, thought feed, and a resume."} />
-  <meta property="og:url" content={window.location.href} />
+  <meta property="og:url" content={base} />
 </svelte:head>
 
-<slot />
+<CentralColumn>
+  <HorizontalNav pages={data.pages.site}/>
+  <div class="flex flex-col grow">
+    <slot></slot>
+  </div>
+  <Footer />
+</CentralColumn>
