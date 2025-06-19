1a4bea8d1e8f8b835564ac6d549ed3822cab579e
Joseph Flinn <joseph.s.flinn@gmail.com>
2025-06-19 09:00:26 -0700

    Clean up routes/posts/+page.js after SSR SSG

--- frontend/src/routes/posts/+page.js
+++ frontend/src/routes/posts/+page.js
@@ -1,18 +1,7 @@
 import { getPosts } from '$lib/posts.js';
 
-export async function load() {
+export const load = async () => {
   return {
     posts: await getPosts()
   };
 }
-
-
-//import { getPostList } from '$lib/utils/loader.js';
-
-//export const load = async ({ fetch }) => {
-//    const response = await fetch('api/posts')
-//    const posts = await response.json()
-//
-//    return { posts }
-//    //return getPostList(fetch);
-//}
